a238f1c2e9a2cfe58bb5e3f9380b550d4c70b4e1
Chris J. Daly <chris.j.daly@gmail.com>
2022-01-02 00:19:20 -0800

    json interlude

--- Java/client/main.sh
+++ Java/client/main.sh
@@ -14,10 +14,9 @@
 ECLIPSE_PLUGINS="/Applications/Eclipse.app/Contents/Eclipse/plugins"
 
 CP_SEP=':' # ';' on Windows
-APACHE_CP="\
-$ECLIPSE_PLUGINS/org.apache.httpcomponents.httpcore_4.4.14.v20210128-2225.jar$CP_SEP\
-$ECLIPSE_PLUGINS/org.apache.httpcomponents.httpclient_4.5.13.v20210128-2225.jar$CP_SEP\
-$ECLIPSE_PLUGINS/org.apache.commons.logging_1.2.0.v20180409-1502.jar"
-
+APACHE_CP=$(find $ECLIPSE_PLUGINS/org.apache.httpcomponents.httpcore_*.jar)$CP_SEP
+APACHE_CP+=$(find $ECLIPSE_PLUGINS/org.apache.httpcomponents.httpclient_*.jar)$CP_SEP
+APACHE_CP+=$(find $ECLIPSE_PLUGINS/org.apache.commons.logging_*.jar)$CP_SEP
+APACHE_CP+=$(find $ECLIPSE_PLUGINS/org.eclipse.json_*.jar)
 
 java -cp "bin${CP_SEP}${APACHE_CP}" Main "$@"
